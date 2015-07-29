9bc2b43ac5acc32e1e8fb82044371b54c9baf51f
maeganw12 <maeganjwilson@gmail.com>
2015-07-28 20:03:39 -0600

    Need to add functions

--- PacMan.java
+++ PacMan.java
@@ -1,20 +1,21 @@
 int sx = 500;
 int sy = 500;
 
+int pacX = 50;
+int pacY = 50;
 
 void setup(){
 	size(sx, sy);
 	smooth(); //Draws everything smooth
+	frameRate(15);
 }
 void draw(){
-	line(5, 5, 255, 5);
-	fill(255, 0, 0);
-	triangle(10, 10, 40, 40, 50, 10);
-	fill(0, 255, 0);
-	quad(100, 100, 130, 100, 160, 180, 50, 180);
-	fill(0, 0, 255);
-	rect(200, 200, 50, 50);
-	ellipse(300, 80, 100, 100);
 	fill(255, 255, 0);
-	arc(300, 300, 50, 50, .79, 5.5);//PacMan!
+	if (pacX == 500){
+		pacX  = 50;
+		pacY += 50;
+	}
+	arc(pacX, pacY, 50, 50, .79, 5.5);//PacMan!
+	ellipse(pacX, pacY, 50, 50);
+
 }
