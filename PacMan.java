2ceb1fe0245af79774a44be6ea447a18ead0900c
maeganw12 <maeganjwilson@gmail.com>
2015-07-28 20:14:31 -0600

    Still not acting how I want

--- PacMan.java
+++ PacMan.java
@@ -7,15 +7,29 @@ int pacY = 50;
 void setup(){
 	size(sx, sy);
 	smooth(); //Draws everything smooth
-	frameRate(15);
+	frameRate(60);
 }
 void draw(){
-	fill(255, 255, 0);
+	erase(pacX-1,pacY-1);
 	if (pacX == 500){
-		pacX  = 50;
+		pacX  = 0;
 		pacY += 50;
 	}
-	arc(pacX, pacY, 50, 50, .79, 5.5);//PacMan!
-	ellipse(pacX, pacY, 50, 50);
+	open(pacX, pacY);
+	pacX++;
 
+}
+
+void open(int x, int y){
+	fill(255, 255, 0);
+	arc(x, y, 50, 50, .79, 5.5);
+}
+void erase(int x, int y){
+	strokeWeight(0);
+	noFill();
+	ellipse(x, y, 50, 50);
+}
+void closed(int x, int y){
+	fill(255, 255, 0);
+	ellipse(x, y, 50, 50);
 }
