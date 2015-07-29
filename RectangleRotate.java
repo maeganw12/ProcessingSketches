4d9b01058149784d08a1b23bc667e97bbe67cf94
maeganw12 <maeganjwilson@gmail.com>
2015-07-28 19:03:05 -0600

    Rectangle moves by following the mouse

--- RectangleRotate.java
+++ RectangleRotate.java
@@ -1,10 +1,19 @@
 int sx = 500;
 int sy = 500;
 
+float angle = 0.0;
+
 
 void setup(){
 	size(sx, sy);
-	fill(0); //No background color
+	//fill(0); //No background color
 	smooth(); //Draws everything smooth
 
+}
+
+void draw(){
+	translate(mouseX, mouseY);//moves by following the mouse
+	rotate(angle);
+	rect(-15, -15, 30, 30);
+	angle += 0.1;
 }
