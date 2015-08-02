6a9ee7285d3103c39d8630fd8cd643ca05c3a39f
maeganw12 <maeganjwilson@gmail.com>
2015-08-02 09:12:57 -0600

    initial commit. has comments for what functions will be needed

---
+++ LineProgram.java
@@ -0,0 +1,12 @@
+//Line Program
+/*
+	Functions Needed:
+		-colorChange ---- will cycle through the colors of the rainbow
+		-moveLeft    ---- all move functions will move (direction) by 10 px
+		-moveRight
+		-moveUp
+		-moveDown
+		-upSideDown  ---- flip the object upside down
+		-spawn       ---- adds another object at mouse position 
+*/
+
